efc6514427733fd4df352c31698bb18491bfb12d
Andrin Bürli <andrin.buerli@csem.ch>
2025-04-09 10:37:50 +0200

    change plotting

--- scripts/render_checkpoint.py
+++ scripts/render_checkpoint.py
@@ -11,11 +11,11 @@ parser.add_argument('--ymin', default=-1.5, help='Minimum y value')
 parser.add_argument('--ymax', default=1.5, help='Maximum y value')
 parser.add_argument('--width', default=512, help='Width of the image')
 parser.add_argument('--height', default=512, help='Height of the image')
-parser.add_argument('--iterations', nargs='+', default=[10], help='List of iterations')
+parser.add_argument('--iterations', nargs='+', default=[5,10,20,40], help='List of iterations')
 parser.add_argument('--zoomed', action='store_true', help='Zoomed in image')
 args = parser.parse_args()
 
-nn = MandelbrotNN(num_hidden_layers=6, hidden_dim=2048)
+nn = MandelbrotNN(num_hidden_layers=6, hidden_dim=2048, log_scale=False)
 nn.load_state_dict(torch.load(args.checkpoint)["state_dict"])
 
 zoom_args = dict(real_min=args.xmin, real_max=args.xmax, imag_min=args.ymin, imag_max=args.ymax)
@@ -28,4 +28,4 @@ for iter in tqdm(args.iterations):
     fig = nn.plot_mandelbrot_set(iterations=iter, **zoom_args)
     
     fig.tight_layout()
-    fig.savefig(f"mandelbrot_{iter}.png", dpi=300)
+    fig.savefig(f"mandelbrot_nn_{iter}.png", dpi=300)
