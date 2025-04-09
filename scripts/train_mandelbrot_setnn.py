62655d79cad312af23e22efa278df9c8984d69a8
Andrin Bürli <andrin.buerli@csem.ch>
2025-04-09 10:39:40 +0200

    pass params from hydra

--- scripts/train_mandelbrot_setnn.py
+++ scripts/train_mandelbrot_setnn.py
@@ -8,7 +8,7 @@ from mbsnn.mandelbrot_setnn import MandelbrotNN
 import pytorch_lightning as pl
 
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 from pathlib import Path
     
 @hydra.main(
@@ -20,8 +20,24 @@ from pathlib import Path
     version_base="1.1",
 )
 def hydra_training(cfg: DictConfig = None) -> None:
-    dataset = MandelbrotDataset(samples=cfg.dataset.num_train_samples, max_iter=cfg.dataset.max_iter, fix_c=False)
-    val_dataset = MandelbrotDataset(samples=cfg.dataset.num_val_samples, max_iter=cfg.dataset.max_iter, fix_c=True)
+    dataset = MandelbrotDataset(
+        samples=cfg.dataset.num_train_samples,
+        max_iter=cfg.dataset.max_iter,
+        fix_c=False,
+        max_magnitude=cfg.dataset.max_magnitude,
+        biased_sampling=cfg.dataset.biased_sampling,
+        real_range=cfg.dataset.real_range,
+        imag_range=cfg.dataset.imag_range,
+        log_scale=cfg.dataset.log_scale)
+    val_dataset = MandelbrotDataset(
+        samples=cfg.dataset.num_val_samples,
+        max_iter=cfg.dataset.max_iter,
+        fix_c=True,
+        max_magnitude=cfg.dataset.max_magnitude,
+        biased_sampling=cfg.dataset.biased_sampling,
+        real_range=cfg.dataset.real_range,
+        imag_range=cfg.dataset.imag_range,
+        log_scale=cfg.dataset.log_scale)
 
     train_loader = DataLoader(dataset, batch_size=cfg.dataset.batch_size, shuffle=True, num_workers=cfg.dataset.num_train_workers, persistent_workers=True)
     val_loader = DataLoader(val_dataset, batch_size=cfg.dataset.batch_size, shuffle=False, num_workers=cfg.dataset.num_val_workers, persistent_workers=True)
@@ -35,12 +51,19 @@ def hydra_training(cfg: DictConfig = None) -> None:
         total_epochs=cfg.train.total_epochs,
         pde_weight=cfg.train.pde_weight,
         max_batch_size=cfg.train.max_batch_size,
+        max_magnitude=cfg.dataset.max_magnitude,
+        time_token_dim=cfg.model.get("time_token_dim", None),
+        compute_dz_dc=cfg.model.compute_dz_dc,
+        compute_dz_dt=cfg.model.compute_dz_dt,
+        log_scale=cfg.dataset.log_scale,
+        pde_weight_warmup_epochs=cfg.train.pde_weight_warmup_epochs,
+        use_clipped_nans=cfg.train.use_clipped_nans,
         output_dir="predictions",)
     
     # add mlflow logger
     lr_monitor = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
     wandb_logger = pl.loggers.WandbLogger(project="mandelbrot-set-nn", entity="andrinburli", log_model=True)
-    
+    wandb_logger.experiment.config.update(OmegaConf.to_container(cfg))
     #wandb_logger.watch(model, log="all", log_freq=1000)
     
     checkpoint_callback = pl.callbacks.ModelCheckpoint(
